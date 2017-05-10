c3155ca71ac54ce0e8718fdcc5d0d92b33aa9166
Chameleonhider <aplubinskas@gmail.com>
2017-05-10 12:12:53 +0300

    first commit

---
+++ spec/atom-s-lang-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomSLangView from '../lib/atom-s-lang-view';
+
+describe('AtomSLangView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
